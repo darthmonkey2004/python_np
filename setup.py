5a0dbdc4cf1eea29a6f434f2998c24ad45942336
Matt McClellan <darthmonkey2004@gmail.com>
2022-05-07 14:19:37 -0500

    asdf

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from distutils.core import setup
-import setuptools
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 setup(name='np',
 	version='1.0',
 	description='NPlayer media player and media management system',
@@ -10,7 +8,7 @@ setup(name='np',
 	url='http://nplayer.simiantech.biz/',
 	packages=['np', 'np.core', 'np.utils'],
 	package_dir={'np': 'np', 'np.core': 'np/core', 'np.utils': 'np/utils', 'np.np': 'np.np'},
-	scripts=['scripts/mkmedialist', 'scripts/np.remote', 'np/main.py'],
+	scripts=['scripts/mkmedialist', 'scripts/np.remote'],
 	data_files=['poster.png'],
-	install_requires=REQUIREMENTS
+	entry_points={'console_scripts': ['np=np.main:start']}
 	)
