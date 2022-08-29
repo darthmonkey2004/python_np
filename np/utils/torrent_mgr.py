a7d72264fc27d3d0f1453e769cbeef3e919e2238
Matt McClellan <darthmonkey2004@gmail.com>
2022-08-29 08:22:54 -0500

    cleanup

--- np/utils/torrent_mgr.py
+++ np/utils/torrent_mgr.py
@@ -1,62 +1,201 @@
-#!/usr/bin/env python3
-
 import subprocess
-
-def get_all():
-	data = subprocess.check_output("transmission-remote -l", stderr=subprocess.STDOUT, shell=True).decode().split("\n")
-	ct = len(data)
-	outlist = []
-	for line in data:
-		outstr = None
-		line = line.strip()
-		chunks = line.split(' ')
-		for chunk in chunks:
-			if chunk != '':
-				if outstr == None:
-					outstr = str(chunk)
-				else:
-					outstr = (outstr + "|" + str(chunk))
-		outlist.append(outstr)
-	torrent_data = {}
-	for line in outlist:
-		data = {}
-		if line is not None:
-			if line.split('|')[0] == 'ID' or line.split('|')[0] == 'Sum:':
-				pass
-			else:
-				chunks = line.split('|')
-				tid = chunks[0]
-				data['percent'] = chunks[1]
-				data['have'] = chunks[2]
-				data['eta'] = chunks[3]
-				data['up'] = chunks[4]
-				data['down'] = chunks[5]
-				data['ratio'] = chunks[6]
-				data['status'] = chunks[7]
-				data['name'] = chunks[8]
-				torrent_data[tid] = data
-	return torrent_data
+from np import readConf,HOME,writeConf,log
 
 
-def get_info(tid, torrent_data=None):
-	com = ("transmission-remote -t" + str(tid) + " --files")
-	lines = subprocess.check_output(com, stderr=subprocess.STDOUT, shell=True).decode().split("\n")
-	outlist = []
-	for line in lines:
-		if line is not None:
-			newline = []
-			outstr = None
-			line = line.strip()
-			chunks = line.split(' ')
-			_list = []
-			for chunk in chunks:
-				if chunk is not None:
-					chunk = str(chunk.strip())
-					if ':' in chunk:
-						chunk = chunk.split(':')[0]
-						if chunk.isnumeric():
-							_list.append(chunk)
-			n = "|"
-			newline = n.join(_list)
-			outlist.append(newline)
+class torrent_mgr():
+	def __init__(self):
+		self.remote_ip = None
+		self.user = HOME.split('/home/')[1]
+
+	def set_remote_host(self, remote_ip=None):
+		conf = readConf()
+		if remote_ip == None:
+			self.remote_ip = conf['pbdl_url']
+		else:
+			self.remote_ip = remote_ip
+			conf['pbdl_url'] = self.remote_ip
+			writeConf(conf)
+		
+
+	def vpn_status(self):
+		com = f"nordvpn status | grep \"Status:\" | cut -d ' ' -f 4"
+		status = subprocess.check_output(com, shell=True).decode().strip()
+		if status == 'Disconnected':
+			return False
+		else:
+			return True
+
+
+	def start_vpn(self):
+		com = f"nordvpn connect"
+		status = subprocess.check_output(com, shell=True).decode().strip()
+		return status
+
+	def stop_vpn(self):
+		com = f"nordvpn disconnect"
+		status = subprocess.check_output(com, shell=True).decode().strip()
+		return status
+
+	def get_data(self):
+		com=f"transmission-remote {self.remote_ip} -l | grep -v \"ID\" | grep -v \"Sum:\""
+		results = subprocess.check_output(com, shell=True).decode().strip().split("\n")
+		tids = []
+		for item in results:
+			tid = item.strip().split(' ')[0]
+			tids.append(tid)
+		self.tdata = {}
+		keys = ['Name', 'Hash', 'Magnet', 'State', 'Location', 'Percent Done', 'ETA', 'Download Speed', 'Upload Speed', 'Have', 'Total size', 'Downloaded', 'Uploaded', 'Ratio', 'Corrupt DL', 'Peers']
+		dkeys = ['name', 'hash', 'magnet', 'state', 'location', 'percent', 'eta', 'download_speed', 'upload_speed', 'have', 'total_size', 'downloaded', 'uploaded', 'ratio', 'corrupt', 'peers']
+		for tid in tids:
+			self.tdata[tid] = {}
+			com = f"transmission-remote {self.remote_ip} -t{tid} --info"
+			data = subprocess.check_output(com, shell=True).decode().strip().split("\n")
+			for line in data:
+				for key in keys:
+					idx = keys.index(key)
+					dkey = dkeys[idx]
+					test = f"{key}: "
+					if test in line:
+						self.tdata[tid][dkey] = line.split(test)[1]
+		return self.tdata
+	
+	def start(self, tid):
+		com=f"transmission-remote {self.remote_ip} -t{tid} -s"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def add(self, magnet):
+		com=f"transmission-remote {self.remote_ip} -t{tid} -a {magnet}"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split(':')[1].split('"')[1]
+		return ret
+
+	def stop(self, tid):
+		com=f"transmission-remote {self.remote_ip} -t{tid} -S"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def start_all(self):
+		j = "\n"
+		out = []
+		data = self.get_data()
+		for tid in list(data.keys()):
+			ret = self.start(tid)
+			out.append(f"{tid}:{ret}")
+		out = j.join(out)
+		return out
+
+	def stop_all(self):
+		j = "\n"
+		out = []
+		data = self.get_data()
+		for tid in list(data.keys()):
+			ret = self.stop(tid)
+			out.append(f"{tid}:{ret}")
+		out = j.join(out)
+		return out
+
+	def set_global_ratio(self, ratio=0):
+		com=f"transmission-remote {self.remote_ip} -gsr {ratio}"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def set_ratio(self, tid, ratio=0):
+		com=f"transmission-remote {self.remote_ip} -t{tid} -sr {ratio}"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def stop_seeds(self):
+		j = "\n"
+		out = []
+		self.set_global_ratio(0)
+		data = self.get_data()
+		for tid in list(data.keys()):
+			ret = self.set_ratio(tid, 0)
+			out.append(f"{tid}:{ret}")
+		out = j.join(out)
+		return out
+
+	def set_start_paused(self):
+		com=f"transmission-remote {self.remote_ip} --start-paused"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+	
+	def set_start_unpaused(self):
+		com=f"transmission-remote {self.remote_ip} --no-start-paused"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def remove(self, tid):
+		com=f"transmission-remote {self.remote_ip} -t{tid} -r"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def remove_and_delete(self, tid):
+		com=f"transmission-remote {self.remote_ip} -t{tid} -rad"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def set_on_finished(self, script_path):
+		if not os.path.exists(script_path):
+			log("Error: Script file not found at {script_path}", 'error')
+			return False
+		com=f"transmission-remote {self.remote_ip} --torrent-done-script \"{script_path}\""
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def remove_on_finished(self):
+		com=f"transmission-remote {self.remote_ip} --no-torrent-done-script"
+		ret = subprocess.check_output(com, shell=True).decode().strip().split('"')[1]
+		return ret
+
+	def start_all_with_vpn(self, host=None):
+		if host is not None:
+			self.set_remote_host(host)
+			log(f"Remote host set to '{host}'.", 'info')
+		state = self.vpn_status()
+		if state == False:
+			log("VPN offline. Starting..", 'info')
+			self.start_vpn()
+			state = self.vpn_status()
+			if state == True:
+				log("VPN Active.", 'info')
+			elif  state == False:
+				log("Error: Unable to activate vpn. Aborting...", 'error')
+				self.stop_all()
+				return False
 				
+		self.start_all()
+		self.stop_seeds()
+		data = self.get_data()
+		ct = len(list(data.keys()))
+		while True:
+			pos = 0
+			for tid in data:
+				percent = data[tid]['percent']
+				if percent == '100%':
+					pos += 1
+			if ct == pos:
+				print (f"Done! ({pos} of {ct})")
+				break
+			else:
+				print (f"Progress: {pos} of {ct}")
+				status = self.vpn_status()
+				if status == False:
+					log(f"VPN not active! Restarting...", 'warning')
+					self.start_vpn()
+				
+		self.stop_all()
+		self.stop_vpn()
+		log(f"All torrents finished! VPN deactivated.", 'info')
+		return True
+				
+if __name__ == "__main__":
+	import sys
+	try:	
+		remote_ip = sys.argv[1]
+	except:
+		remote_ip = input("Enter remote host ip: ")
+	mgr = torrent_mgr()
+	mgr.set_remote_host(remote_ip)
+	mgr.start_all_with_vpn()
+	
