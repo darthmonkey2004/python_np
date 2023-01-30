f9fc2bc0983d27530f00b2a1db1b8d070cd7baf6
Matt McClellan <darthmonkey2004@gmail.com>
2023-01-30 09:39:09 -0600

    Removed old versions and unused files, basic cleanup.

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='np',
 	url='http://nplayer.simiantech.biz/',
 	packages=['np', 'np.core', 'np.utils', 'np.ws'],
 	package_dir={'np': 'np', 'np.core': 'np/core', 'np.utils': 'np/utils', 'np.np': 'dist/np.bin'},
-	scripts=['scripts/install_np.sh', 'scripts/mkmedialist', 'scripts/np.remote', 'scripts/write_client_html'],
+	scripts=['scripts/install_np.sh', 'scripts/mkmedialist', 'scripts/np.remote', 'scripts/write_client_html', 'np/np'],
 	data_files=['images/poster.png', 'images/np.jpeg', 'images/np.png', 'README', 'requirements.txt', 'version.txt'],
 	)
